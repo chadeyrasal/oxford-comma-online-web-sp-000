f9bd86fc8f8d0860b7f31e24f4f54959c23399ec
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 19:10:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,9 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array1 = ["array[0]", "array[1]"]
+    array1 = []
+    array1 << array[0]
+    array1 << array[0]
     string1 = array1.join(", ")
     string2 = " and " + array.last
     final = string1 + string2
