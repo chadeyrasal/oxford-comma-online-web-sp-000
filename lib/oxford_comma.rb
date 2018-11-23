95121eb29f2d624a9f89b53468912e76a8088306
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 19:30:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,8 +14,9 @@ def oxford_comma(array)
   elsif array.size > 3
     array1 = []
     counter = 0
-    while counter < array.size - 1
+    while counter < array.size - 1 do
       
+      counter +=1
     end
   end
 end
