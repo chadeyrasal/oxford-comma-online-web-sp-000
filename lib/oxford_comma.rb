08bc5de8ba383991ca0bd48673bf75a26182ae4d
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 19:05:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array1 = 
+    array1 = array[0] + array[1]
+    array1.join(", ")
   elsif array.size > 3
   end
 end
